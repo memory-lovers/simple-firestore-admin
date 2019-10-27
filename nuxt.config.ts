d200449e1cfa98ae9be92d8a409bbc88c1b93787
Y.Fujiura <memory.loverz@gmail.com>
2019-10-27 23:26:17 +0900

    init typescript support and buefy

---
+++ nuxt.config.ts
@@ -0,0 +1,66 @@
+import { Configuration } from "@nuxt/types";
+
+const config: Configuration = {
+  mode: "spa",
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || "",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || ""
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
+  /*
+   ** Global CSS
+   */
+  css: ["~/assets/css/buefy.scss", "~/assets/css/transition.scss"],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: ["@nuxt/typescript-build"],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    "nuxt-buefy"
+  ],
+
+  /**
+   * nuxt-buefy
+   * Doc: https://github.com/buefy/nuxt-buefy
+   * Doc: https://buefy.org/documentation/constructor-options
+   */
+  buefy: {
+    css: false
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config heres
+     */
+    extend(config, ctx) {}
+  }
+};
+
+export default config;
